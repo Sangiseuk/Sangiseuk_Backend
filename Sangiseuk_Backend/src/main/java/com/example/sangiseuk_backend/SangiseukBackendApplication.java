ba04600c3c7807566efba21c9e4a0919427c278d
이용태 <ioosulsa@gmail.com>
2024-11-12 19:57:30 +0900

    init spring

---
+++ Sangiseuk_Backend/src/main/java/com/example/sangiseuk_backend/SangiseukBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sangiseuk_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangiseukBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangiseukBackendApplication.class, args);
+	}
+
+}
---
+++ Sangiseuk_Backend/src/test/java/com/example/sangiseuk_backend/SangiseukBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sangiseuk_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangiseukBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
